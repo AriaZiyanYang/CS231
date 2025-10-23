0d41b31376408b206e15165b834ef1d74e5e3e02
Ziyan Yang <95478486+AriaZiyanYang@users.noreply.github.com>
2025-10-23 15:05:13 -0700

    Add training and test data for KNN
    Added training and testing data for KNN classification.

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -184,9 +184,34 @@ class KNearestNeighbor(object):
       count = vote.most_common()
       y_pred[i] = count[0][0]
 
+
       #########################################################################
       #                           END OF YOUR CODE                            #
       #########################################################################
 
     return y_pred
 
+# 三类：0,1,2
+X_train = np.array([
+    [0.0, 0.0],   # 0
+    [0.1, 0.0],   # 0
+    [5.0, 5.0],   # 1
+    [5.1, 5.0],   # 1
+    [2.5, 2.5],   # 2
+    [2.6, 2.6],   # 2
+])
+y_train = np.array([0, 0, 1, 1, 2, 2])
+
+X_test = np.array([
+    [0.05, 0.05],  # 0 类附近
+    [5.05, 5.05],  # 1 类附近
+    [2.55, 2.55],  # 2 类附近
+    [2.7, 2.4],    # 更靠近 2，但 k=3 时观察投票
+])
+
+knn = KNearestNeighbor()
+knn.train(X_train, y_train)
+
+pred = knn.predict(X_test, k=3, num_loops=1)
+print("Case 2 preds:", pred)
+# 期望（直觉）: [0, 1, 2, 2]  —— 第4个点对 2 的邻居更多
